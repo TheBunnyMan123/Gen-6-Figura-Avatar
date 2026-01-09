5f44f6ea63136437d43a27bbff77ff449cd2dfdf
TheBunnyMan123 <bunny@tkbunny.net>
2026-01-08 19:49:14 -0600

    throbber

--- scripts/common/model.lua
+++ scripts/common/model.lua
@@ -2,7 +2,8 @@ vanilla_model.PLAYER:setVisible(false)
 vanilla_model.CAPE:setVisible(false)
 
 models.models.model.root.Head.Eyes:setPrimaryRenderType("EMISSIVE")
-
+models.models.model.root.Head.Throbber:setColor(1, 0.5, 0)
+	:setPrimaryRenderType("EMISSIVE")
 
 if avatar:getUUID() == "1dcce150-0064-4905-879c-43ef64dd97d7" then return end
 
@@ -11,4 +12,6 @@ tex:fill(2, 3, 1, 4, vec(0, 1, 1))
 tex:fill(5, 3, 1, 4, vec(0, 1, 1))
 tex:fill(9, 9, 6, 6, vec(0, 0.1, 0.1))
 tex:update()
+
+models.models.model.root.Head.Throbber:setColor(0, 1, 1)
 
---
+++ scripts/common/throbber.lua
@@ -0,0 +1,23 @@
+local throbber_part = models.models.model.root.Head.Throbber
+local eyes = models.models.model.root.Head.Eyes
+throbber = {}
+
+local throbbing = 0
+function throbber.add()
+	throbbing = throbbing + 1
+end
+
+function throbber.sub()
+	throbbing = throbbing - 1
+end
+
+
+local tick = 0
+function events.TICK()
+	throbber_part:setVisible(throbbing >= 1)
+	eyes:setVisible(throbbing < 1)
+	if throbbing < 1 then return end
+	tick = tick + 1
+	throbber_part.Throbber:setUVPixels(0, (math.floor(tick / 2) % 8) * 11)
+end
+
