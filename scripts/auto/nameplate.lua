3c61a5caf5a0a8c0a374e62a22d1621d4525dce9
TheBunnyMan123 <bunny@tkbunny.net>
2026-03-18 10:17:28 -0500

    the stupid

--- scripts/auto/nameplate.lua
+++ scripts/auto/nameplate.lua
@@ -3,7 +3,7 @@ nameplate.ENTITY:setVisible(false)
 	:setOutlineColor(vectors.hexToRGB("#154020"))
 
 local colors = {
-	"#804000",
+	"#A05000",
 	"#FF8000",
 	"#FFA000"
 }
@@ -102,33 +102,82 @@ local info = {
 	{"Time Zone", ":flag_us: CT"}
 }
 
-local nameText = "\xE2\x9C\xA8Bunny\xE2\x9C\xA8"
-nameplate.ALL:setText(toJson {
-	text = nameText,
-	hoverEvent = {
-		action = "show_text",
-		value = hover
-	},
-	hover_event = {
-		action = "show_text",
-		value = hover
+local nameText = "\xE2\x9C\xA8TheKillerBunny\xE2\x9C\xA8"
+local name = {}
+
+for char in string.gmatch(nameText, "([%z\1-\127\128-\255][\128-\191]*)") do
+	name[#name + 1] = char
+end
+
+local entityTasks = {}
+local width = 0
+local holder = models.models.model:newPart("NAMEPLATE", "CAMERA"):setPrimaryRenderType("EMISSIVE")
+for i = 1, #name do
+	entityTasks[#entityTasks+1] = {
+		char = name[i],
+		task = holder:newText("task" .. i):setText(toJson {
+			text = name[i],
+			color = colors[2]
+		}):setPos(-width, 0):setScale(3/8):setLight(15):setOutline(true),
+		offset = i,
+		prevWidth = width
 	}
-})
+
+	width = width + client.getTextWidth(name[i] or "") * (3/8)
+end
+
+for _, v in pairs(entityTasks) do
+	v.task:setPos(v.task:getPos().x + width / 2, 0)
+end
+holder:setPivot(0, 40, 0)
 
 local tick = 0
-local plate = models.models.model.root.Nameplate
-plate:setSecondaryRenderType("EMISSIVE")
-:setSecondaryTexture("CUSTOM", textures["textures.nameplate"])
-:setParentType("CAMERA")
-plate.FrontLayer:setColor(vectors.hexToRGB(colors[2]))
-plate.BackLayer:setColor(vectors.hexToRGB(colors[1]))
+
+local json = {}
+for k, v in ipairs(entityTasks) do
+	local text = {
+		text = v.char,
+		color = colors[2],
+		hoverEvent = {
+			action = "show_text",
+			value = hover
+		},
+		hover_event = {
+			action = "show_text",
+			value = hover
+		}
+	}
+	json[#json + 1] = text
+end
+avatar:store("color", "FF8000")
+nameplate.ALL:setText(toJson(json))
 
 function events.TICK()
 	tick = tick + 1
-	plate:setVisible(client.isHudEnabled())
+
+	local json = {}
+	for k, v in ipairs(entityTasks) do
+		local text = {
+			text = v.char,
+			color = colors[2],
+		}
+		v.task:setText(toJson(text)):setOutlineColor(0.125, 0.0625, 0):setSeeThrough(not player:isSneaking())
+		json[#json + 1] = text
+		json[#json].hoverEvent = {
+			action = "show_text",
+			value = hover
+		}
+		json[#json].hover_event = json[#json].hoverEvent
+	end
+	nameplate.ALL:setText(toJson(json))
 end
 
 function events.RENDER(delta)
-	plate.FrontLayer:setPos(0, math.sin(math.lerp(tick - 1, tick, delta) / 10), 0)
-	plate.BackLayer:setPos(0, math.sin(math.lerp(tick - 3, tick - 2, delta) / 10) * 1.2, 0)
+	for _, v in pairs(entityTasks) do
+		v.task:setPos(-v.prevWidth + width / 2, math.lerp(
+			math.sin((tick + v.offset - 1) / 4),
+			math.sin((tick + v.offset) / 4),
+			delta
+		)):setVisible(not player:isInvisible() and client.isHudEnabled())
+	end
 end
