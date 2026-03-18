3454130050c7127b3d0cf8566136317b2a9aec37
TheBunnyMan123 <bunny@tkbunny.net>
2026-03-17 19:40:34 -0500

    dumb parsing was dumb

--- preprocess.lua
+++ preprocess.lua
@@ -1,7 +1,6 @@
 local pre_utils = require("libs.TheKillerBunny.preprocess_utils")
 local async = require("libs.TheKillerBunny.async")
 
-local parser = require("libs.dumbParser")
 local scripts = listFiles("", true)
 local conf = pre_utils.config ---@type PreprocessUtils.Config
 
@@ -17,17 +16,6 @@ async.forpairs(scripts, function( _, v)
 	if conf.strip_host_only and flags.host_only then
 		remove[#remove + 1] = v
 		return
-	end
-
-	local tokens = parser.tokenize(script)
-	local parsed = parser.parse(tokens)
-	local final
-	if conf.minify then
-		final = parser.minify(parsed, conf.optimize)
-		silly_backports:addScript(v, parser.toLua(final))
-	elseif conf.optimize then
-		final = parser.optimize(parsed)
-		silly_backports:addScript(v, parser.toLua(final))
 	end
 end, function()
 	async.forpairs(remove, function(_, v)
